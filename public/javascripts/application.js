bd6bcdb455f41871928b1a90c4becaae6f9ec03a
David Tuite <dtuite@gmail.com>
2011-05-04 14:20:38 +0100

    changed payment range to slider functionality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
-
   $( "button, input:submit, a").button();
   $( "a", ".demo" ).click(function() { return false; });
 });
