47b23a424170b7c824c13eae3a89d64ce0230a90
David Tuite <dtuite@gmail.com>
2011-05-08 20:36:49 +0100

    this one is defo gonna work

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -1,10 +1,15 @@
+# make it auto scale the workers
+# make it run though houses in the database, updating information
+
 class Scrape
-#  calling Scrape.new.perform is running this directly, not delayed
-  def perform(county_id = 30)
-    county_names = %w[Dublin Meath Kildare Wicklow Longford Offaly Westmeath Laois Louth Carlow Kilkenny Waterford
+  def county_names
+    @county_names = %w[Dublin Meath Kildare Wicklow Longford Offaly Westmeath Laois Louth Carlow Kilkenny Waterford
         Wexford Kerry Cork Clare Limerick Tipperary Galway Mayo Roscommon Sligo Leitrim Donegal Cavan
         Monaghan Antrim Armagh Tyrone Fermanagh Derry Down].freeze
-
+  end
+  
+#  calling Scrape.new.perform is running this directly, not delayed
+  def perform(county_id = 30)
     url = "http://www.daft.ie/searchsale.daft?s%5Bcc_id%5D=c#{county_id}&s%5Ba_id%5D%5B%5D=&s%5Broute_id%5D=&s%5Ba_id_transport%5D=0&s%5Baddress%5D=&s%5Btxt%5D=&s%5Bmnb%5D=&s%5Bmxb%5D=&s%5Bmnp%5D=&s%5Bmxp%5D=&s%5Bpt_id%5D=&s%5Bhouse_type%5D=&s%5Bsqmn%5D=&s%5Bsqmx%5D=&s%5Bmna%5D=&s%5Bmxa%5D=&s%5Bnpt_id%5D=&s%5Bdays_old%5D=&s%5Bnew%5D=&s%5Bagreed%5D=&search.x=34&search.y=20&search=Search+%BB&more=&tab=&search=1&s%5Bsearch_type%5D=sale&s%5Btransport%5D=&s%5Badvanced%5D=&s%5Bprice_per_room%5D=&fr=default"
     puts "Scraping #{county_names[county_id.to_i - 1]} via it's Daft county ID: #{county_id}..."
 
@@ -13,7 +18,7 @@ class Scrape
 
     while(agent.page.link_with(:text => "Next Page \u00BB")) do
       agent.page.search(".content").each do |search_result|
-        store search_result
+        store search_result, county_id
       end
 
       agent.page.link_with(:text => "Next Page \u00BB").click
@@ -21,7 +26,7 @@ class Scrape
   end
 end
 
-def store item
+def store item, county_id
 # I don't want to scrape houses with no prices ie. 'POA' or the like
   title = item.at(".title a")
   if item.at(".price").text[/[0-9,]+/]
